e2cb05098724ab0bf78736a2b81aa10de091a8c1
Simeon <adegbolasimeon@gmail.com>
2020-07-26 13:47:29 +0100

    persist userName to database

--- models/user.model.js
+++ models/user.model.js
@@ -27,6 +27,10 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    userName: {
+      type: String,
+      required: true,
+    },
     bio: {
       type: String,
     },
